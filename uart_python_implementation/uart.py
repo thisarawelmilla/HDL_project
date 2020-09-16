6796eb2ff58c29aa6c5fb44fe2b71102c83fb744
Uzama Zaid <uzamazaid@outlook.com>
2020-09-16 13:54:27 +0530

    created uart python

--- uart_python_implementation/uart.py
+++ uart_python_implementation/uart.py
@@ -1,63 +1,40 @@
-from serial import Serial
+import serial
+import io
+import time
+from PIL import Image
+import numpy as np
+
+IMAGE_PATH = "image.jpg"
+IMAGE_SIZE = 10
 
 
-class SerialCommunication:
-    timer = ''
-    buffer = b''
-    ser = ''
-
-    def __init__(self):
-        pass
-
-    def cancel_timer(self):
-        try:
-            self.timer.cancel()
-        except Exception as ex:
-            template = "ct: An exception of type {0} occured. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-
-    def open_serial(self, COM, Baud, Parity, sBits, dBits, Timeout):
-        try:
-            self.ser = Serial(port=COM, baudrate=Baud, parity=Parity,
-                         stopbits=sBits, bytesize=dBits, timeout=Timeout)
-        except Exception as ex:
-            template = "op: An exception of type {0} occured. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-
-    def send_serial(self, data):
-        try:
-            if self.ser.is_open:
-                s = str(data)
-                chars = []
-                for c in s:
-                    chars.append(ord(c))
-                    chars = list(map(int, chars))
-                self.ser.write(chars)
-                self.ser.flush()
-        except Exception as ex:
-            template = "ss: An exception of type {0} occured. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-
-    def read_serial(self, tmr, period):
-        try:
-            if self.ser.inWaiting() > 0:
-                __stb = self.ser.read(self.ser.inWaiting())
-                self.buffer += __stb
-        except Exception as ex:
-            template = "rs: An exception of type {0} occured. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-        if not tmr:
-            return
-        self.timer = threading.Timer(period, self.read_serial, [True, period])
-        self.timer.start()
-        return
+# create serial connection
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM14'
+ser.open()
 
 
-mySer = SerialCommunication()
-mySer.open_serial('COM7', 19200, 'N', 1, 8, 0)
-data = '010111010001'
-mySer.send_serial(data)
+# create image array
+image = Image.open(IMAGE_PATH, mode='r')
+image = image.convert(mode='L')
+image = np.array(image.resize((IMAGE_SIZE, IMAGE_SIZE)), np.int64)
+image = np.array(image)
+image = image.flatten()
+
+# send pixels as byte
+for i in range(len(image)):
+	ser.write(bytes([image[i]]))
+
+# receive filtered image
+received_image = []
+
+for i in range(IMAGE_SIZE ** 2):
+	pixel_byte = ser.read(1)
+	received_image.append(int.from_bytes(pixel_byte, "big"))
+
+# show received image
+received_image = np.array(received_image)
+received_image = np.resize(received_image, (IMAGE_SIZE, IMAGE_SIZE))
+image = Image.fromarray(received_image)
+image.show()
